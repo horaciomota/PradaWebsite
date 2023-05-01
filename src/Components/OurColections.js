46543ad805aff5af587c192a6f15584a938c35e6
Horaci Mota <horaciomota93@gmail.com>
2023-05-01 16:23:31 +0100

    colection loop added.

--- src/Components/OurColections.js
+++ src/Components/OurColections.js
@@ -1,4 +1,27 @@
 const OurColections = () => {
+  const colection = [
+    {
+      picture:
+        "https://www.prada.com/content/dam/pradanux/home_page/2023/03/Tropico/ww/men_bag/banner_MB.jpg",
+      title: "Holiday 2023",
+      id: "1",
+    },
+
+    {
+      picture:
+        "https://www.prada.com/content/dam/pradanux/home_page/2023/03/Tropico/kw_ca_br_au/hero/banner_MB.jpg",
+      title: "Spring/summer 2024",
+      id: "2",
+    },
+
+    {
+      picture:
+        "https://www.prada.com/content/dam/pradanux/home_page/2023/03/Tropico/kw_ca_br_au/hero/banner_MB.jpg",
+      title: "Spring/summer 2024",
+      id: "2",
+    },
+  ];
+
   return (
     <section className="max-w-5xl mx-auto p-4 ">
       <div className="flex ">
@@ -17,18 +40,24 @@ const OurColections = () => {
             ideas
           </p>
         </div>
-        <div className="flex flex-col w-colection overflow-hidden ">
-          <div className="h-colection w-colection bg-red-900">
-            <img
-              src="https://media.cdn.adultswim.com/uploads/20220927/thumbnails/2_229271558509-RickAndMorty_607_DeSmithation.png"
-              className="object-cover h-colection"
-              alt="Image Colection"
-            />
-          </div>
-          <h1>Holiday 2023</h1>
-          <p className="tracking-tight text-gray-900 uppercase text-center lg:text-center text-2xl lg:text-6xl mt-5 mb-5">
-            01
-          </p>
+
+        {/* MAP OF COLECTION */}
+        <div className="flex w-full overflow-hidden bg-lime-800">
+          {colection.map((card) => (
+            <div key={card.id} className="  ">
+              <div className="h-colection w-colection">
+                <img
+                  src={card.picture}
+                  className="object-cover mx-auto  "
+                  alt="Image Colection"
+                />
+              </div>
+              <h1 className="text-left">{card.title}</h1>
+              <p className="tracking-tight uppercase text-center lg:text-center text-2xl lg:text-6xl mt-5 mb-5">
+                {card.id}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
